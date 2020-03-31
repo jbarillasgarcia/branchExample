e100c58e66b0020ce02d6d415935a2d26b94cf30
usr_gsm <cgarciam@tigo.com.gt>
2020-03-30 20:40:40 -0600

    Este es el primer cambio en mi rama branch2.

--- branchExample1/src/main/java/gt/edu/umg/ingenieria/sistemas/is/branchexample1/BranchApp.java
+++ branchExample1/src/main/java/gt/edu/umg/ingenieria/sistemas/is/branchexample1/BranchApp.java
@@ -3,7 +3,7 @@ package gt.edu.umg.ingenieria.sistemas.is.branchexample1;
 public class BranchApp {
     
     public static void main(String[] args) {
-        System.out.println("Ahora es la rama master (3er cambio).");                
+        System.out.println("Ahora es la rama branch2.");                
     }
     
 }
