0f40eaa276c84b0fc48592791331ac15dd3f8ec2
usr_gsm <cgarciam@tigo.com.gt>
2020-03-30 20:21:53 -0600

    Initial version of branch example.

---
+++ branchExample1/src/main/java/gt/edu/umg/ingenieria/sistemas/is/branchexample1/BranchApp.java
@@ -0,0 +1,9 @@
+package gt.edu.umg.ingenieria.sistemas.is.branchexample1;
+
+public class BranchApp {
+    
+    public static void main(String[] args) {
+        System.out.println("Hello world!!!");                
+    }
+    
+}
